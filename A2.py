957035c8143dca80452aeeec2618aa2c72155f66
olivia <osa12@sfu.ca>
2025-01-31 15:42:56 -0800

    randomization

--- A2.py
+++ A2.py
@@ -1,31 +1,34 @@
+print("############NEW RUN############")
 # import libraries
 
 import turtle
 turtle.colormode(255)
 import random
-
 import tkinter as tk
 
-# Create a root window (hidden)
+# getting screen w and h
 root = tk.Tk()
-root.withdraw()  # Hide the root window
-
-# Get screen width and height
+root.withdraw() 
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 
-# Create a screen object
+# making turtle screen
 screen = turtle.Screen()
 screen.setup(width, height)
-screen.tracer(0)  # Disable automatic screen updates for manual control
 screen.bgcolor("black")
 
-
-
-# Create a turtle object
+# making turtles
+t0 = turtle.Turtle()
 t1 = turtle.Turtle()
 t2 = turtle.Turtle()
 t3 = turtle.Turtle()
+t4 = turtle.Turtle()
+t5 = turtle.Turtle()
+t6 = turtle.Turtle()
+t7 = turtle.Turtle()
+t8 = turtle.Turtle()
+t9 = turtle.Turtle()
+t10 = turtle.Turtle()
 
 
 # FUNCTION to generate l-system instructions based on axiom and rules.
@@ -39,40 +42,56 @@ def create_l_system(iterations, axiom, rules):
     return result
 
 
+## FUNCTION to set up turtle objects ##
+# Parameters:
+# - t: turtle object
+# - coords: initial coordinates
+# - turtle speed
+def setup_turtle(t, coords, speed, size):
+        t.hideturtle()
+        t.pensize(size)
+        t.speed(speed)  
+        t.penup()
+        t.goto(coords[0], coords[1])  
+        t.pendown()
+        print("setting up turtle")
+
+
 # FUNCTION to assign colour to pen
 def set_pen_color(c, t):
     t.pencolor(c[0], c[1], c[2])
+    # print("setting pen colour")
 
 
-# Draw the L-System using turtle graphics.
-# Parameters:
-# - instructions: string of L-System commands
-# - angle: turning angle in degrees
-# - distance: forward movement distance
+# FUNCTION to draw l-system using turtle graphics
+def draw_l_system(instructions, angle, distance, color, amt, size):
 
-def draw_l_system(instructions, angle, distance, color, turtle):
-    stack = []  # for saving previous state
-
-    t = turtle 
+    stack0 = []
+    stack1 = []
+    stack2 = []
+    stack3 = []
+    stack4 = []
+    stack5 = []
     
-    i = 1
+    i = 1   # increment amount
     rInc, gInc, bInc = True, True, True
-    
-    sizeInc = True
-    penSize = 4
-    
-    # speedInc = True
-    # penSpeed = "fastest"
     
     r = color[0]
     g = color[1]
     b = color[2]
 
+    # set up turtles
+    for k in range(amt):
+        turtle = globals().get(f't{k}')  # Access turtle t1, t2, etc. dynamically
+        print(f't{k}')
+        set_pen_color((r, g, b), turtle)
+        setup_turtle(turtle, (random.randint(-300, 300), random.randint(-300, 300)), "fastest", size)
+
+    ## loop runs for each instruction character
     for cmd in instructions:
 
         ## smoothly modifying colours ##
-        # increment r value
-        if rInc:
+        if rInc:    # increment r value
           r +=i
           if r >= 255:
             r = 255
@@ -83,8 +102,7 @@ def draw_l_system(instructions, angle, distance, color, turtle):
             r = 0
             rInc = True
 
-        # increment g value
-        if gInc:
+        if gInc:    # increment g value
           g +=i
           if g >= 255:
             g = 255
@@ -95,8 +113,7 @@ def draw_l_system(instructions, angle, distance, color, turtle):
             g = 0
             gInc = True
 
-        # increment blue
-        if bInc:
+        if bInc:    # increment blue
           b +=i
           if b >= 255:
             b = 255
@@ -107,140 +124,163 @@ def draw_l_system(instructions, angle, distance, color, turtle):
             b = 0
             bInc = True
 
-        set_pen_color((r, g, b), t)
-
-
-        # ## randomly adjusting pen size ##
-        # chance = random.randint(0, 5)
-        
-        # if sizeInc == True:
-        #         penSize += i
-        #         # penSpeed +=i
-        #         if penSize >= 13:
-        #             penSize = 13
-        #             sizeInc = False
-        #             print("inc: ",penSize)
-        # else:
-        #         penSize -= i
-        #         # penSpeed -= i
-        #         if penSize <= 4:
-        #             penSize = 4
-        #             sizeInc = True
-        #             print("dec: ",penSize)
-                    
-        # if chance > 3:
-        #     i = random.randint(0, 1)
-            
-        # t.pensize(penSize)
-        
-        # if penSpeed >= 20:
-        #     penSpeed = 20
-        # elif penSpeed <= 13:
-        #     penSpeed = 13
-            
-        # t.speed("fastest")  
-        
+        for k in range(amt):
+            turtle = globals().get(f't{k}')  # Access turtle t1, t2, etc. dynamically
+            if turtle:
+                set_pen_color((r, g, b), turtle)
 
         ## drawing the L-system ##
         if cmd == 'F':  # Move forward and draw
-            t.forward(distance)
+            for k in range(amt):
+                turtle = globals().get(f't{k}') 
+                if turtle:
+                    turtle.forward(distance)
+            print("forward")
+
         elif cmd == 'f':  # Move forward without drawing
-            t.penup()
-            t.forward(distance)
-            t.pendown()
+            for k in range(amt):
+                turtle = globals().get(f't{k}')  
+                if turtle:
+                    turtle.penup()
+                    turtle.forward(distance)
+                    turtle.pendown()
+            print("forward no draw")
+
         elif cmd == '+':  # Turn right
-            t.right(angle)
+            for k in range(amt):
+                turtle = globals().get(f't{k}') 
+                if turtle:
+                    turtle.right(angle)
+            print("right")
+
         elif cmd == '-':  # Turn left
-            t.left(angle)
+            for k in range(amt):
+                turtle = globals().get(f't{k}') 
+                if turtle:
+                    turtle.left(angle)
+            print("left")
+
         elif cmd == '[':  # Save current state (recursion)
-            stack.append((t.position(), t.heading()))
+            for k in range(amt):
+                turtle = globals().get(f't{k}') 
+                if turtle:
+                    stack = globals().get(f'stack{k}')
+                    print(f'stack{k}')
+                    if stack:
+                        stack.append((turtle.position(), turtle.heading()))
+            print("save")
+
         elif cmd == ']':  # Restore previous state (recursion)
-            position, heading = stack.pop()
-            t.penup()
-            t.goto(position)
-            t.setheading(heading)
-            t.pendown()
+            for k in range(amt):
+                stack = globals().get(f'stack{k}')
+                if stack:
+                    position, heading = stack.pop()
+                    turtle = globals().get(f't{k}')
+                    if turtle:
+                        turtle.penup()
+                        turtle.goto(position)
+                        turtle.setheading(heading)
+            print("previous state")
             
-        print("running", r)
+        # print("running", r)
 
     # Close the window on click
     screen.exitonclick()
 
-def setup_turtle(t, coords, speed):
-        t.hideturtle()
-        t.speed(speed)  # Fastest speed
-        t.penup()
-        t.goto(coords[0], coords[1])  # Start position
-        t.pendown()
-        print("setting up turtle")
-        print(coords)
 
 
-## ACTUAL USE
 
-# koch
-# axiom = "F"
-# rules = {"F" : "F+F-F-F+F"}
-# iterations = 4
-# angle = 90
-# lineSize = 10
 
-# sierpinski triangle
-# axiom = "F-G-G"
-# rules = {"F" : "F-G+F+G-F", "G" : "GG"}
-# iterations = 5
-# angle = 120
-# lineSize = 10
 
-# dragon curve
-# axiom = "FX"
+
+## DRAWING
+
+axiom = "+F"
+# rules = {"F":"FF+FF+FF++"}
+
+# rules = {"F":"FF+F-F"} #looks like cityscape
+
+# rules = {"F":"F+F-F"} #looks like fractal-ish
+
+# rules = {"F":"F[+FFFFF]-F+F"} #looks like buildingish
+
 # rules = {
-#     "X":"X+YF+",
-#     "Y":"-FX-Y"
-# }
-# angle = 90
-# iterations = 12
-# lineSize = 20
+#    "F":"F+X-F",
+#    "X":"F+[XXX]F-"
+#    } 
+# #looks like buildingish
 
-# axiom = "F-G"
 # rules = {
-#     "F" : "F-G+F+G-F", 
-#     "G" : "GG+F"
-# }
-
-# AS OF NOW
-# axiom = "F-G-F"
-# rules = {
-#     "F" : "G+G-G+G+G-G", 
-#     "G" : "FF"
-# }
-
-axiom = "F"
-rules = {"F":"FF+FF+FF++"}
-iterations = 1
-angle = 75
-scale = 10
+#    "F":"+FX-FX",
+#    "X":"F+[XXX]F-"
+#    } 
 
 
-setup_turtle(t1, (0, 0), 13)
-setup_turtle(t2, (200, 0), 13)
-setup_turtle(t3, (-200, -0), 13)
 
 
-# make parameters into instructions
-instructions = create_l_system(iterations, axiom, rules)
+# define L system grammar
+gen_grammar = {
+    'LSYSTEM': [["ACTION"]],
+    'DELIMIT': [":"],
+    'AXIOM': ["F"],
+    'ACTION': ["F", "f", "-", "+"]
+}
 
+for i in range(random.randint(1,10)):
+    gen_grammar['LSYSTEM'][0].append("ACTION")
+
+# print(gen_grammar)
+
+
+def generate(symbol, grammar):
+    if isinstance(symbol, str) and symbol in grammar:
+        production = random.choice(grammar[symbol])
+        if isinstance(production, list):
+            return ''.join(generate(sym, grammar) for sym in production)
+        return production
+    return symbol
+
+rules_string = generate('LSYSTEM', gen_grammar)
+print(rules_string)
+
+rules_dict = {}
+rules_dict[gen_grammar['AXIOM'][0]] = rules_string
+print(rules_dict)
+
+
+
+
+#returns all as list
+# def generate(symbol, grammar):
+#     result = []  # List to store the individual results
+#     if isinstance(symbol, str) and symbol in grammar:
+#         production = random.choice(grammar[symbol])  # Randomly choose a production
+#         if isinstance(production, list):
+#             for sym in production:
+#                 result.extend(generate(sym, grammar))  
+#         else:
+#             result.append(production)
+#     else:
+#         result.append(symbol) 
+#     return result
+
+
+
+# rules = {"F":"F+X-F"}
+
+
+iterations = random.randint(1, 8)
+angle = random.randint(45, 90)
+distance = random.randint(5, 20)
+
+size = random.randint(1, 13)
+amount = random.randint(1, 9)
 # generate startcolor
 startColor = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-# draw l system
-draw_l_system(instructions, angle, scale, startColor, t1)
-draw_l_system(instructions, angle, scale, startColor, t2)
-draw_l_system(instructions, angle, scale, startColor, t3)
 
-screen.update()
+# make parameters into instructions
+# instructions = create_l_system(iterations, axiom, rules)
 
-
-# for i in range(5):
-#   t.pensize(i+1)
-#   draw_l_system(instructions, angle, i)
+   
+# draw_l_system(instructions, angle, distance, startColor, amount, size)
